07d0822f3a30477b19c4225a591f8265399a19b6
Ivo Tasevski <ivo.tasevski@qinshift.com>
2024-03-20 10:35:47 +0100

    Dummy commit

--- serviceB/src/main/java/com/ivotasevski/observability/ServiceBApplication.java
+++ serviceB/src/main/java/com/ivotasevski/observability/ServiceBApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ServiceBApplication {
 
+    // MAin
     public static void main(String[] args) {
         SpringApplication.run(ServiceBApplication.class, args);
     }
